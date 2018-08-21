8f4e85ca1d2cdacd74b308a8a5cce7c52ee57843
searching9x <searching9x@yahoo.com>
2018-08-21 20:26:51 +0700

    AngularJS Commit

--- angularjs/hello/hello-angularjs.js
+++ angularjs/hello/hello-angularjs.js
@@ -1,16 +1,29 @@
-// Create a Application object.
+// Create a Application named "myApp".
 var app = angular.module("myApp", []);
 
 // Create a Controller named "myCtrl"
 app.controller("myCtrl", function($scope) {
 
+    $scope.operation = "+";
 
-    $scope.firstName = "Donald";
-    $scope.lastName= "Trump";
+    $scope.variable1 = 30;
+    $scope.variable2 = 20;
+    $scope.result = 0;
 
+    $scope.setOperationSum = function()  {
+       $scope.operation = "+";
+    }
 
-    $scope.fullName = function()  {
-      return $scope.firstName + " " + $scope.lastName;
+    $scope.setOperationMinus = function()  {
+       $scope.operation = "-";
+    }
+
+    $scope.calculate = function()  {
+       if($scope.operation == "+") {
+          $scope.result = parseFloat($scope.variable1)  + parseFloat($scope.variable2);
+       } else if($scope.operation == "-") {
+          $scope.result = parseFloat($scope.variable1)  - parseFloat($scope.variable2);
+       }
     }
 
 });
