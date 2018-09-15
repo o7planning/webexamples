d0ac9716a6ed54a76cead183a48e083335631325
o7planning <o7planning@users.noreply.github.com>
2018-09-15 15:49:54 +0700

    AngularJS Commit

---
+++ angularjs/validation/ng-pattern-example.js
@@ -0,0 +1,26 @@
+var app = angular.module("myApp", []);
+
+app.controller("myCtrl", function($scope) {
+
+    $scope.pinCode = "";
+
+    // Show more error infos.
+    function printErrorInfo() {
+        console.log($scope.myForm.$error); 
+    }
+
+    $scope.checkOnSubmit = function(event) {
+        if ($scope.myForm.$invalid) {
+            alert("Something invalid!");
+
+            printErrorInfo();
+
+            // Cancel submit
+            event.preventDefault();
+            return false;
+        }
+        alert("All valid => Submit now!");
+        return true;
+    }
+
+});
