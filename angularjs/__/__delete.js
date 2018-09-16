88edf91a4369aea1ae649c64b20dd70366b29388
o7planning <o7planning@users.noreply.github.com>
2018-09-16 21:25:20 +0700

    AngularJS Commit

--- angularjs/__/__delete.js
+++ angularjs/__/__delete.js
@@ -1,26 +1,7 @@
 var app = angular.module("myApp", []);
 
-var ctrl = app.controller("myCtrl", function($scope) {
+app.controller("myCtrl", function($scope) {
 
-  $scope.todoDatas = [
-    {name: 'Create a custom directive', completed: true},
-    {name: 'Learn about restrict', completed: true},
-    {name: 'Master scopes', completed: false}
-  ];
+    $scope.fullName = "";
 
 });
-
-// Create a Directive named "myTodoList"
-// E: Element <my-todo-list todo-list="=" title="@">
-
-app.directive("myTodoList", function() {
-    return {
-        restrict : "E",
-        templateUrl : "todo-template.html",
-
-        scope: {
-          todoList: '=',
-          title: '@'
-        }
-    };
-}); 
--- angularjs/validation/$submitted-example.js
+++ angularjs/validation/$submitted-example.js
@@ -4,13 +4,9 @@ app.controller("myCtrl", function($scope) {
 
     $scope.field1 = "";
 
-    $scope.checkOnSubmit = function(event) {
+    $scope.doSubmitForm = function(event) {
         alert("OK: " + $scope.myForm.$submitted);
-
-        // $scope.myForm.$submitted = true;
-        // Cancel submit
-        //event.preventDefault();
-        //return false;
-        return true;
+        // Code to Submit form!
     }
+
 });
