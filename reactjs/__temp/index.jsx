48344801f379770cb752916d39d941e0b25d81de
o7planning <o7planning@users.noreply.github.com>
2018-09-21 16:20:37 +0700

    ReactJS Commit

---
+++ reactjs/__temp/index.jsx
@@ -0,0 +1,63 @@
+// The Header creates links that can be used to navigate
+// between routes.
+const Header = () => (
+  <header>
+    <nav>
+      <ul>
+        <li><Link to='/'>Home</Link></li>
+        <li><Link to='/roster'>Roster</Link></li>
+        <li><Link to='/schedule'>Schedule</Link></li>
+      </ul>
+    </nav>
+  </header>
+);
+
+
+const Home = () => (
+  <div>
+    <h1>Welcome to the Tornadoes Website!</h1>
+  </div>
+);
+
+const Player = (props) => {
+  const player = PlayerAPI.get(
+    parseInt(props.match.params.number, 10)
+  )
+  if (!player) {
+    return <div>Sorry, but the player was not found</div>
+  }
+  return (
+    <div>
+      <h1>{player.name} (#{player.number})</h1>
+      <h2>Position: {player.position}</h2>
+      <Link to='/roster'>Back</Link>
+    </div>
+  )
+};
+
+const Roster = () => (
+  <Switch>
+    <Route exact path='/roster' component={FullRoster}/>
+    <Route path='/roster/:number' component={Player}/>
+  </Switch>
+);
+
+const Schedule = () => (
+  <div>
+    <ul>
+      <li>6/5 @ Evergreens</li>
+      <li>6/8 vs Kickers</li>
+      <li>6/14 @ United</li>
+    </ul>
+  </div>
+);
+
+const Main = () => (
+  <main>
+    <Switch>
+      <Route exact path='/' component={Home}/>
+      <Route path='/roster' component={Roster}/>
+      <Route path='/schedule' component={Schedule}/>
+    </Switch>
+  </main>
+);
--- reactjs/router/router-example.jsx
+++ reactjs/router/router-example.jsx
@@ -1,49 +1,80 @@
+// import {BrowserRouter,NavLink} from 'react-router-dom';
+// const { BrowserRouter, NavLink, Route } = ReactRouterDOM;
+
+
+// Component
+class ProductShortInfo extends React.Component {
+  render() {
+    return (
+      <div className="product">
+         <h3>Samsung Galaxy S9</h3>
+         <p>Price: $900</p>
+      </div>
+    );
+  }
+}
+
+// Component
+class ProductFeature extends React.Component {
+  render() {
+    return <h3>Something about this Product!</h3>;
+  }
+}
+
+// Component
+class ProductImages extends React.Component {
+  render() {
+    return <h3>Some Images of this Product</h3>;
+  }
+}
+
+// Component
+class ProductComments extends React.Component {
+  render() {
+    return <h3>Some Customer Comments</h3>;
+  }
+}
+
+
+//
+class Product extends React.Component {
+  render() {
+    return (
+      <div id="dashboard">
+        <ProductShortInfo/>
+        <div className="menu">
+          <ReactRouterDOM.NavLink exact to="/">
+             Feature
+          </ReactRouterDOM.NavLink>
+
+          <ReactRouterDOM.NavLink exact to="/automation">
+             Images
+          </ReactRouterDOM.NavLink>
+
+          <ReactRouterDOM.NavLink exact to="/marketing">
+             Comments
+          </ReactRouterDOM.NavLink>
+
+        </div>
+        <div className="router-display-area">
+          <ReactRouterDOM.Route exact path="/" component={ProductFeature} />
+          <ReactRouterDOM.Route exact path="/marketing" component={ProductComments} />
+          <ReactRouterDOM.Route exact path="/automation" component={ProductImages} />
+        </div>
+      </div>
+    );
+  }
+}
 //
 class App extends React.Component {
-
-
   render() {
     return (
-       <div>App</div>
+      <ReactRouterDOM.BrowserRouter>
+        <Product />
+      </ReactRouterDOM.BrowserRouter>
     );
   }
 }
 
-//
-class CreateApp extends React.Component {
-
-
-  render() {
-    return (
-       <div>CreateApp</div>
-    );
-  }
-}
-
-//
-class CreateApp2 extends React.Component {
-
-
-  render() {
-    return (
-       <div>CreateApp2</div>
-    );
-  }
-}
-
-
-
-var routes = (
-   <ReactRouter.BrowserRouter>
-      <ReactRouter.Route name="app" path="/" component={App}/>
-      <ReactRouter.Route name="app" path="author" component={CreateApp}/>
-      <ReactRouter.Route name="app" path="author2" component={CreateApp2}/>
-  </ReactRouter.BrowserRouter>
-);
-
-var hashHistory = ReactRouter.hashHistory;
-
-ReactDOM.render(
-  <ReactRouter.BrowserRouter>  <ReactRouter.Router history={hashHistory}>{routes}</ReactRouter.Router></ReactRouter.BrowserRouter>,
-    document.getElementById('router1')
-);
+// Render
+ReactDOM.render(<App />, document.querySelector("#app"));
