e84f6acd018f0aa662dfc73dab00bcc60b0e55b9
o7planning <o7planning@users.noreply.github.com>
2018-11-24 15:24:36 +0700

    ECMAScript

---
+++ node-ecmascript/module/es6node-file1.mjs
@@ -0,0 +1,9 @@
+// Import *
+import * as myModule from './es6node-module-file.mjs';
+
+console.log( myModule.HELLO ); // Hello Everybody
+console.log( myModule.BYE ); // Goodbye!
+
+myModule.sayHello("Tom")  ; // Hello Tom
+
+myModule.sayGoodbye("Tom")  ;// Goodbye Tom
