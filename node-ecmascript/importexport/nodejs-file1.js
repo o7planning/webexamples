f531cfb929448abbeec8897ce6eecae5e5247385
o7planning <o7planning@users.noreply.github.com>
2018-11-24 12:16:37 +0700

    ECMAScript

---
+++ node-ecmascript/importexport/nodejs-file1.js
@@ -0,0 +1,9 @@
+// Import
+var myModule = require("./nodejs-module-file.js");
+
+console.log( myModule.HELLO ); // Hello Everybody
+console.log( myModule.BYE ); // Goodbye!
+
+myModule.sayHello("Tom")  ; // Hello Tom
+
+myModule.sayGoodbye("Tom")  ;// Goodbye Tom
