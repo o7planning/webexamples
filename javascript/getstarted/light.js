64e348156f3fb66d4e7c105e3d6c61ebbfa922e2
o7planning <o7planning@users.noreply.github.com>
2019-02-09 12:42:36 +0700

    Javascript

---
+++ javascript/getstarted/light.js
@@ -0,0 +1,9 @@
+function turnLightOn()  {
+  var img = document.getElementById("myImage");
+  img.src = "light-on.gif";
+}
+
+function turnLightOff()  {
+  var img = document.getElementById("myImage");
+  img.src = "light-off.gif";
+}
