82f44edef60e045840af41127a1aff5388ebb7aa
o7planning <o7planning@users.noreply.github.com>
2019-02-10 21:51:39 +0700

    ECMAScript

---
+++ node-ecmascript/buildin-function/setTimeout-example.js
@@ -0,0 +1,13 @@
+
+function sayHello()  {
+   console.log("Hello Everyone!");
+}
+
+console.log("3");
+console.log("2");
+console.log("1");
+console.log("Call setTimeout!");
+
+setTimeout(sayHello, 3000); // 3 seconds
+
+console.log("End!");
