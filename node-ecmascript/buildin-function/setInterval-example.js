f27efa257f4be6e56f6639d8960acaa41c02b863
o7planning <o7planning@users.noreply.github.com>
2019-02-11 20:49:55 +0700

    ECMAScript

--- node-ecmascript/buildin-function/setInterval-example.js
+++ node-ecmascript/buildin-function/setInterval-example.js
@@ -2,10 +2,10 @@
 console.log("3");
 console.log("2");
 console.log("1");
-console.log("Call setTimeout!");
+console.log("Call setInterval!");
 
-setTimeout( function() {
-  console.log("Hello Everyone!");
+setInterval( function() {
+   console.log("Hello Everyone!");
 }, 3000); // 3 seconds
 
 console.log("End!");
