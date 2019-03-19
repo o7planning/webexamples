430086a535dce806a0cb9b15192d116197fbaa8a
o7planning <o7planning@users.noreply.github.com>
2019-03-20 00:47:05 +0700

    Javascript

--- javascript/keyevents/prop-shiftKey-example.js
+++ javascript/keyevents/prop-shiftKey-example.js
@@ -2,9 +2,9 @@
 function keydownHandler(evt)  {
 
    if(evt.shiftKey) {
-      document.getElementById("log-div").innerHTML ="evt.keyEvent = true";
+      document.getElementById("log-div").innerHTML ="evt.shiftKey = true";
    } else {
-      document.getElementById("log-div").innerHTML ="evt.keyEvent = false";
+      document.getElementById("log-div").innerHTML ="evt.shiftKey = false";
    }
 
 }
