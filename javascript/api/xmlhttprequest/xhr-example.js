8d64137dfa67f4a54c5df45fb6b1b9a49a6a72dc
o7planning <o7planning@users.noreply.github.com>
2019-04-07 16:47:51 +0700

    Javascript

--- javascript/api/xmlhttprequest/xhr-example.js
+++ javascript/api/xmlhttprequest/xhr-example.js
@@ -12,31 +12,34 @@ function clickHandler(evt) {
     // Initialize It.
     xhr.open("GET", URL, async);
 
-
-    // Send it (Without body data)
-    xhr.send();
-
     resetLog();
 
     xhr.onprogress = function(progressEvent) {
         appendLog("onprogress! " + progressEvent);
     }
 
+    // readyState (State of request):
+    // 0 - XMLHttpRequest.UNSENT
+    // 1 - XMLHttpRequest.OPENED
+    // 2 - XMLHttpRequest.HEADERS_RECEIVED
+    // 3 - XMLHttpRequest.LOADING
+    // 4 - XMLHttpRequest.DONE
+    xhr.onreadystatechange = function(event)  {
+        appendLog("onreadystatechange! readyState = " + xhr.readyState);
+        appendLog(" status = " + xhr.status);
+        appendLog(" statusText = " + xhr.statusText);
+    }
+
     xhr.onload = function(progressEvent) {
-        // 0 - XMLHttpRequest.UNSENT
-        // 1 - XMLHttpRequest.OPENED
-        // 2 - XMLHttpRequest.HEADERS_RECEIVED
-        // 3 - XMLHttpRequest.LOADING
-        // 4 - XMLHttpRequest.DONE
-        appendLog("readyState = " + xhr.readyState);
-        appendLog("status = " + xhr.status);
-        appendLog("statusText = " + xhr.statusText);
+        appendLog("onload!");
+        appendLog(" status = " + xhr.status);
+        appendLog(" statusText = " + xhr.statusText);
 
         if (xhr.status == 200) {
-            appendLog(" ---------------- xhr.responseText ----------------: ");
+            appendLog(" ------ xhr.responseText ------: ");
             appendLog(xhr.responseText);
 
-            appendLog(" ---------------- xhr.responseXML ----------------: ");
+            appendLog(" ------ xhr.responseXML -------: ");
             appendLog(xhr.responseXML); // [object XMLDocument]
 
             // Convert XMLDocument to String.
@@ -46,9 +49,13 @@ function clickHandler(evt) {
     }
 
     xhr.onerror = function(progressEvent) {
+        appendLog("onerror!");
         appendLog("Has Error!");
     }
-} 
+
+    // Send it (Without body data)
+    xhr.send();
+}
 
 function resetLog() {
     document.getElementById('textarea-log').value = "";
