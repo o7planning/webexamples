f3cef282e501a47343eac3439c96a80593899cd2
o7planning <o7planning@users.noreply.github.com>
2019-05-19 02:56:53 +0700

    ECMAScript

---
+++ node-ecmascript/promise/function-example.js
@@ -0,0 +1,9 @@
+
+function sum(a, b)  {
+  return a + b;
+}
+
+// Call function:
+let result = sum(10, 5);
+
+console.log(c);
---
+++ node-ecmascript/promise/promise-example.js
@@ -0,0 +1,44 @@
+// [ECMAScript 5 Syntax]
+
+var isNetworkOK = true;
+
+// This function return a Promise
+function downloadFile(url)  {
+
+    // A Promise
+    var willIGetAFile = new Promise (
+        function (resolve, reject) {
+
+            if (isNetworkOK) {
+                var file = {
+                    fileName: 'file.mp3',
+                    fileSize: '3 MB'
+                };
+                resolve(file); // fulfilled
+            } else {
+                var error = new Error('There is a problem with the network.');
+                reject(error); // reject
+            }
+
+        }
+    );
+
+    return willIGetAFile; // Return a Promise.
+}
+
+// Call downloadFile(..) function:
+// Returns a Promise object:
+var willIGetAFile = downloadFile("http://example.com/file.mp3");
+
+
+willIGetAFile
+        .then(function (fulfilled) {
+            // Get a File
+            // Output: {fileName: 'file.mp3', fileSize: '3 MB'}
+            console.log(fulfilled);
+        })
+        .catch(function (error) {
+             // Network Error!
+             // Output: There is a problem with the network.
+             console.log(error.message);
+        });
