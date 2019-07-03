338475cda74a096fc240aa42d2e73e617f0df0c0
o7planning <o7planning@users.noreply.github.com>
2019-07-03 16:39:13 +0700

    Javascript

---
+++ javascript/fetch/get-text-example-way1.js
@@ -0,0 +1,32 @@
+
+// A URL returns TEXT data.
+var url = "https://rawgit.com/o7planning/webexamples/master/_testdatas_/simple-text-data.txt";
+
+
+function doGetTEXT()  {
+
+  // Call fetch(url) with default options.
+  // It returns a Promise object (Resolve response object)
+  var aPromise = fetch(url);
+
+  // Work with Promise object:
+  aPromise
+    .then(function(response) {
+        console.log("OK! Server returns a response object:");
+        console.log(response);
+
+        response.text()
+          .then(function(myText)) {
+               console.log("Text:");
+               console.log(myText);
+          })
+          .catch(function(error)) {
+             // Never happened.
+          }
+    })
+    .catch(function(error)  {
+        console.log("Noooooo! Something error:");
+        // Network Error!
+        console.log(error);
+    }); 
+}
