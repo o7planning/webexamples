4f26cbd690b8514c900e494ca1544b40509cb87a
o7planning <o7planning@users.noreply.github.com>
2019-08-31 00:40:39 +0700

    Commit

--- javascript/urlencoding/encodeURIAll.js
+++ javascript/urlencoding/encodeURIAll.js
@@ -1,23 +1,6 @@
 
-function encodeURIAll(text)  {
-    return text.replace(/./g, function(aCharacter) {
-
-       // Characters is not encoded by encodeURIComponent(): . ! ~ * ' ( )
-       if(aCharacter === '.') {
-          return "%2e";
-       } else if(aCharacter === '!') {
-          return "%21";
-       } else if(aCharacter === '~') {
-          return "%7e";
-       } else if(aCharacter === '*') {
-          return "%2a";
-       } else if(aCharacter === "'") {
-          return "%27";
-       } else if(aCharacter === "(") {
-          return "%28";
-       } else if(aCharacter === ")") {
-          return "%29";
-       }
-       return encodeURIComponent(aCharacter);
-    });
+function encodeURIAll(text) {
+  return encodeURIComponent(text).replace(/[!'()~*]/g, (c) => {
+    return '%' + c.charCodeAt(0).toString(16);
+  });
 }
