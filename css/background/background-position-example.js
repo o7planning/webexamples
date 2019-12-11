aa44005b6a250d3b19780aa22ea8f0b7dc9042e4
o7planning <o7planning@users.noreply.github.com>
2019-12-11 16:42:05 +0700

    CSS

---
+++ css/background/background-position-example.js
@@ -0,0 +1,12 @@
+function setBgPosition(value) {
+   var myElement = document.getElementById("my-div");
+   myElement.style.backgroundPosition = value;
+}
+function setBgOrigin(value) {
+   var myElement = document.getElementById("my-div");
+   myElement.style.backgroundOrigin = value;
+}
+function setBgRepeat(value) {
+   var myElement = document.getElementById("my-div");
+   myElement.style.backgroundRepeat = value;
+}
